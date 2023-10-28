3ebb86aba41872a75a0b790165949e8cddc8b962
jt trinh <Nhattd@selex.vn>
2023-10-28 17:37:57 +0700

    template method design pattern

---
+++ lib/student_xml_cacuclar.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_design_pattern/xml_student_api.dart';
+
+class StudentXMLBMICacular {
+  const StudentXMLBMICacular({this.api = const XmlStudentsApi()});
+
+  final XmlStudentsApi api;
+
+  
+}
